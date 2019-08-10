7422cc453830f2e3ed9fe602be35e66512e42ce4
Gustav <sir.gustav.the.coder@gmail.com>
2019-08-10 16:38:07 +0200

    added assets and created something

---
+++ scripts/fish.gd
@@ -0,0 +1,24 @@
+extends Sprite
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+var fishes = [
+	preload("res://assets/sprites/fish01.png"),
+	preload("res://assets/sprites/fish02.png"),
+	preload("res://assets/sprites/fish03.png"),
+	preload("res://assets/sprites/fish04.png"),
+	preload("res://assets/sprites/fish05.png")
+	]
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var type = randi() % 5
+	print(type)
+	self.texture = fishes[type]
+	# self.set_texture(fishes[type])
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
