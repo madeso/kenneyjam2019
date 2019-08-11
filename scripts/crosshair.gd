0be30b4a4103e1092bb84d60e939ecbd3b55c43c
Gustav <sir.gustav.the.coder@gmail.com>
2019-08-11 11:17:39 +0200

    found a way to change cursor center, use cursor now instead of sprite

--- scripts/crosshair.gd
+++ scripts/crosshair.gd
@@ -1,4 +1,4 @@
-extends Sprite
+extends Node
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -9,12 +9,12 @@ var loading = load("res://assets/sprites/crosshair-loading.png")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	# Input.set_custom_mouse_cursor(crosshair)
+	# Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	Input.set_custom_mouse_cursor(crosshair, 0, Vector2(21, 21))
 	# couldnt find a way to move cursor to center
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	self.position = get_viewport().get_mouse_position()
 	if Input.is_mouse_button_pressed(BUTTON_LEFT):
 		pass
